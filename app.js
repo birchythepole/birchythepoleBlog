dbe6c4b4f6ce882654f1c46b31125c674c602004
Paul <pawelbrzozowski91@gmail.com>
2020-07-25 21:54:26 +0200

    Initial Commit, setup all project files for futue develoopment

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae ex vulputate, rhoncus massa non, vehicula ligula. Aliquam blandit non sapien quis hendrerit. Mauris at neque vel nisl porta fringilla nec nec leo. Ut at ex eu urna varius rutrum in non mi. Donec a purus enim. Sed hendrerit ultrices lobortis. Vestibulum tortor dui, auctor quis vehicula ut, dignissim non ante. Praesent et lorem sagittis, congue risus sit amet, faucibus dolor. Fusce facilisis justo et commodo ultricies. Fusce quis mauris vulputate, lacinia odio vitae, ultricies arcu.";
+const aboutContent =
+  "Sed imperdiet sodales feugiat. Aenean aliquam faucibus sapien, ac tincidunt tortor vulputate egestas. Sed vestibulum congue ipsum id interdum. Ut at dui quis odio mollis fringilla efficitur sed arcu. Cras vel vestibulum sapien, sit amet condimentum neque. Cras ullamcorper, arcu non dictum convallis, sem ante maximus magna, scelerisque venenatis lorem nibh in urna. Donec blandit dui id tincidunt dignissim. Sed cursus tincidunt erat, quis cursus ipsum dignissim sit amet. Proin sed interdum orci, non volutpat neque. Praesent nulla libero, fermentum id rutrum vel, porta eget orci. Proin volutpat lacus nulla, a mattis dolor pulvinar quis. Nullam id rutrum tellus. Fusce lorem dui, placerat nec posuere et, rutrum sed lectus. Nunc eu lacinia ligula.";
+const contactContent =
+  "Sed sit amet sem ut mauris vulputate cursus accumsan vel lorem. Aliquam nec metus non justo faucibus tempus. Pellentesque egestas est velit, at facilisis ligula viverra sit amet. Nunc aliquam, ante quis vehicula varius, dolor magna elementum dolor, a mollis magna sem eu libero. Aliquam nec neque nisl. Nulla consequat arcu euismod turpis cursus gravida. Aliquam mollis tristique finibus. Maecenas a arcu metus. Proin hendrerit venenatis auctor. Quisque urna eros, consequat et tincidunt ac, congue id velit. Vivamus egestas, lorem sit amet efficitur cursus, lacus turpis volutpat ante, id fringilla turpis neque vel dolor.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
